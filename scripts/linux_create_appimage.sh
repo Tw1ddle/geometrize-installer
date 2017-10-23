d0bad2aa5fde08bc2b7fd48268010743214a71e0
sam <sam@silver42.karoo.co.uk>
2017-10-23 19:05:47 +0100

    Tweak linux deploy script

--- scripts/linux_create_appimage.sh
+++ scripts/linux_create_appimage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -x
-
 # Download linuxdeployqt
-wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt
-chmod +x linuxdeployqt
+wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" -O linuxdeployqt
+chmod a+x linuxdeployqt
 
-# Work around https://github.com/probonopd/linuxdeployqt/issues/28
-sudo ln -s /usr/lib/x86_64-linux-gnu/qt5/plugins /usr/lib/plugins
+unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
+
+# TODO copy svg plugin dll?
 
 # Create the AppImage
 mkdir bundle
 cp geometrize bundle/geometrize
-./linuxdeployqt bundle/geometrize -appimage -bundle-non-qt-libs -verbose=2
+./linuxdeployqt bundle/geometrize -bundle-non-qt-libs -verbose=2
+./linuxdeployqt bundle/geometrize -appimage
