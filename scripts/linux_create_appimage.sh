01593198be7e46d6e805989accede1d86563f596
sam <sam@silver42.karoo.co.uk>
2017-10-23 23:42:59 +0100

    Tweak build script (it seems to work now)

--- scripts/linux_create_appimage.sh
+++ scripts/linux_create_appimage.sh
@@ -13,4 +13,8 @@ yes | cp Geometrize appimage/geometrize
 ./linuxdeployqt appimage/geometrize -bundle-non-qt-libs -verbose=2
 ./linuxdeployqt appimage/geometrize -appimage
 
+# Move it ready for CI deployment stage to pick it up
 mv Geometrize-x86_64.AppImage Geometrize.AppImage
+
+# Make it executable
+chmod +x Geometrize.AppImage
