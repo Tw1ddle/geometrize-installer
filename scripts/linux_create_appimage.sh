41364d5ddf9e30203f7fe956cd308b4eab3fb4da
sam <sam@silver42.karoo.co.uk>
2017-10-23 23:17:11 +0100

    Tweak build script

--- scripts/linux_create_appimage.sh
+++ scripts/linux_create_appimage.sh
@@ -8,27 +8,9 @@ chmod a+x linuxdeployqt
 
 unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
 
-# TODO copy svg plugin dll?
-
 # Create the AppImage
-mkdir -p appimage
 yes | cp Geometrize appimage/geometrize
 ./linuxdeployqt appimage/geometrize -bundle-non-qt-libs -verbose=2
 ./linuxdeployqt appimage/geometrize -appimage
 
-pwd
-ls -a
-
-pushd appimage
-
-ls -a
-
-popd
-
-pushd scripts
-
-ls -a
-
-popd
-
-mv scripts/Geometrize-x86_64.AppImage appimage/Geometrize.AppImage
+mv Geometrize-x86_64.AppImage Geometrize.AppImage
