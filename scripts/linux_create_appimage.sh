f5b2f2ede9252b04e772a5075069e116d042b34f
sam <sam@silver42.karoo.co.uk>
2018-05-25 19:28:47 +0100

    Tweak appimage script

--- scripts/linux_create_appimage.sh
+++ scripts/linux_create_appimage.sh
@@ -13,11 +13,15 @@ yes | cp Geometrize appdir/geometrize
 # Run linuxdeployqt
 ./linuxdeployqt appdir/geometrize -bundle-non-qt-libs -verbose=2
 
-# Workaround so the AppImage runs on systems that ship old libstdc++ like Ubuntu 14.04 (https://github.com/Tw1ddle/geometrize/issues/5)
-mkdir -p appdir/usr/optional/
-wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so" -O ./appdir/usr/optional/exec.so
-mkdir -p appdir/usr/optional/libstdc++/
-cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./appdir/usr/optional/libstdc++/
+# Workaround so the AppImage runs on systems that ship old gcc (and so older libstdc++/libgcc)
+# like Ubuntu 14.04 (see https://github.com/Tw1ddle/geometrize/issues/5)
+mkdir -p appdir/optional/
+mkdir -p appdir/optional/libstdc++/
+cp /lib/x86_64-linux-gnu/libstdc++.so.6 ./appdir/optional/libstdc++/
+mkdir -p appdir/optional/libgcc_s/
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 ./appdir/optional/libgcc_s/
+wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so" -O ./appdir/optional/exec.so
+
 pushd appdir
 rm AppRun
 wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64" -O AppRun
@@ -26,7 +30,6 @@ popd
 
 # Create the AppImage itself
 ./linuxdeployqt --appimage-extract
-
 export PATH=$(readlink -f ./squashfs-root/usr/bin):$PATH
 NAME=$(grep '^Name=.*' appdir/geometrize.desktop | cut -d "=" -f 2 | sed -e 's|\ |_|g')
 ./squashfs-root/usr/bin/appimagetool -g ./appdir/ Geometrize-x86_64.AppImage
