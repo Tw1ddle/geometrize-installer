d2b83b8cdee08f755b0ae1a9d5729a7528d68944
sam <sam@silver42.karoo.co.uk>
2018-05-25 20:05:05 +0100

    Tweak appimage script

--- scripts/linux_create_appimage.sh
+++ scripts/linux_create_appimage.sh
@@ -13,7 +13,10 @@ yes | cp Geometrize appdir/geometrize
 # Run linuxdeployqt
 ./linuxdeployqt appdir/geometrize -bundle-non-qt-libs -verbose=2
 
-# Workaround so the AppImage runs on systems that ship old gcc (and so older libstdc++/libgcc)
+# Extract the base appimage
+./linuxdeployqt --appimage-extract
+
+# Workarounds so the AppImage runs on systems that ship old gcc (and so older libstdc++/libgcc)
 # like Ubuntu 14.04 (see https://github.com/Tw1ddle/geometrize/issues/5)
 mkdir -p appdir/optional/
 mkdir -p appdir/optional/libstdc++/
@@ -21,20 +24,12 @@ cp /lib/x86_64-linux-gnu/libstdc++.so.6 ./appdir/optional/libstdc++/
 mkdir -p appdir/optional/libgcc_s/
 cp /lib/x86_64-linux-gnu/libgcc_s.so.1 ./appdir/optional/libgcc_s/
 wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so" -O ./appdir/optional/exec.so
-
+# Replace AppRun with the patched one
 pushd appdir
 rm AppRun
 wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64" -O AppRun
 chmod a+x AppRun
 popd
 
-# Create the AppImage itself
-./linuxdeployqt --appimage-extract
-export PATH=$(readlink -f ./squashfs-root/usr/bin):$PATH
-NAME=$(grep '^Name=.*' appdir/geometrize.desktop | cut -d "=" -f 2 | sed -e 's|\ |_|g')
-./squashfs-root/usr/bin/appimagetool -g ./appdir/ Geometrize-x86_64.AppImage
-
-ls -a
-
-# Move it ready for CI deployment stage to pick it up
-mv Geometrize-x86_64.AppImage Geometrize.AppImage
+# Pack everything back into an AppImage
+./squashfs-root/usr/bin/appimagetool -g ./appdir/ Geometrize.AppImage
