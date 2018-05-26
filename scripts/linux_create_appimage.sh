56fb1ce4bba2c9cbbbd954ffef992c599c0d33b7
sam <sam@silver42.karoo.co.uk>
2018-05-26 04:44:14 +0100

    Try using the patched-up apprun and copying libraries to usr/optional/ again

--- scripts/linux_create_appimage.sh
+++ scripts/linux_create_appimage.sh
@@ -12,14 +12,30 @@ chmod a+x linuxdeployqt
 
 unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
 
-# Create the AppImage
+# Run linuxdeployqt on the built executable
 cp Geometrize appdir/geometrize
 ./linuxdeployqt appdir/geometrize -bundle-non-qt-libs -verbose=2
 
 # Also ship the libstdc++ library with the AppImage
 # Assumes we're building this with gcc-8 and with a particular version of libstdc++
-cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.25 appdir/lib/libstdc++.so.6
+ls -a /usr/lib/x86_64-linux-gnu/
 
+mkdir -p appdir/usr/optional/
+mkdir -p appdir/usr/optional/libstdc++/
+
+cp /usr/lib/x86_64-linux-gnu/libgcc_s.so.1 ./appdir/usr/optional/libgcc_s/
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.25 ./appdir/usr/optional/libstdc++/libstdc++.so.6
+wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so" -O ./appdir/usr/optional/exec.so
+
+# Get a patched-up version of AppRun that ensures that the latest libstdc++ library is used
+# This should cover edge-cases like when the system libstdc++ is newer than the bundled one
+pushd appdir
+rm AppRun
+wget -c "https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64" -O AppRun
+chmod a+x AppRun
+popd
+
+# Create the AppImage
 mksquashfs appdir geometrizesquashed -root-owned -noappend
 cat runtime >> Geometrize.AppImage
 cat geometrizesquashed >> Geometrize.AppImage
