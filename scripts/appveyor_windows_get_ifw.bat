383e73f905033cc6c912a996ad71fb142a410d7b
sam <samcodesit@gmail.com>
2021-10-31 12:35:29 +0000

    Try updating AppVeyor CI for latest IFW and MSVC2019, update submodule

--- scripts/appveyor_windows_get_ifw.bat
+++ scripts/appveyor_windows_get_ifw.bat
@@ -4,7 +4,7 @@ setlocal
 mkdir ifw
 
 @echo Download Qt Installer Framework for Windows
-curl -L -O https://download.qt.io/official_releases/qt-installer-framework/3.0.1/QtInstallerFramework-win-x86.exe
+curl -L -O https://download.qt.io/official_releases/qt-installer-framework/4.1.1/QtInstallerFramework-windows-x86-4.1.1.exe
 
 @echo Unpack the installer framework
-7z x QtInstallerFramework-win-x86.exe -oifw -aoa
+7z x QtInstallerFramework-windows-x86-4.1.1.exe -oifw -aoa
